e71a0dcdce54c3ad1e45366b0144e1442cf34604
fred-monier <frederico.monier@gmail.com>
2017-08-22 21:23:31 -0300

    Aula prática 2017 08 22

--- app/src/main/java/ibratec/recife/pe/br/helloworld/MainActivity.java
+++ app/src/main/java/ibratec/recife/pe/br/helloworld/MainActivity.java
@@ -1,13 +1,64 @@
 package ibratec.recife.pe.br.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editToastValue;
+    Button buttonToast;
+    Button buttonStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editToastValue  = (EditText) findViewById(R.id.editText);
+
+        buttonToast = (Button) findViewById(R.id.button1);
+        buttonToast.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Toast toast = Toast.makeText(getApplicationContext(), editToastValue.getText(), Toast.LENGTH_SHORT);
+                   toast.show();
+               }
+           }
+        );
+
+        buttonStudent = (Button) findViewById(R.id.button2);
+        buttonStudent.setOnClickListener(new StudentClick(this));
+        /*
+        buttonStudent.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                   Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+                   startActivity(intent);
+              }
+          }
+        );
+        */
+    }
+
+
+    private class StudentClick implements View.OnClickListener {
+
+        MainActivity mAct;
+
+        public StudentClick(MainActivity mAct) {
+            super();
+            this.mAct = mAct;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(this.mAct, StudentActivity.class);
+            startActivity(intent);
+        }
     }
 }

3430d677d06fee60f4b72a91f12eff535f2923a9
fred-monier <frederico.monier@gmail.com>
2017-08-29 21:43:45 -0300

    Aula prática 2017 08 29

--- app/src/main/java/ibratec/recife/pe/br/helloworld/MainActivity.java
+++ app/src/main/java/ibratec/recife/pe/br/helloworld/MainActivity.java
@@ -1,23 +1,45 @@
 package ibratec.recife.pe.br.helloworld;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String VAR_NOME_STUDENT = "nomeStudent";
+
+    private static final int INSERIR_ESTUDANTE = 1;
+    private static final String LISTA_ESTUDANTES = "listaStudents";
 
     EditText editToastValue;
     Button buttonToast;
     Button buttonStudent;
 
+    ArrayList<String> listaStudents;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            listaStudents = savedInstanceState.getStringArrayList(LISTA_ESTUDANTES);
+            this.montarLista();
+        } else {
+            listaStudents = new ArrayList<String>();
+        }
 
         editToastValue  = (EditText) findViewById(R.id.editText);
 
@@ -45,6 +67,41 @@ public class MainActivity extends AppCompatActivity {
         */
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(LISTA_ESTUDANTES, listaStudents);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.opcoes_estudante, menu);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == INSERIR_ESTUDANTE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+
+                String nomeStudent = data.getStringExtra(MainActivity.VAR_NOME_STUDENT);
+                listaStudents.add(nomeStudent);
+                this.montarLista();
+            }
+        }
+    }
+
+    private void montarLista() {
+        ArrayAdapter<String> listaStudentsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1
+                ,listaStudents);
+        ListView listaStudentsView = (ListView) findViewById(R.id.listview1);
+        listaStudentsView.setAdapter(listaStudentsAdapter);
+
+        registerForContextMenu(listaStudentsView);
+    }
 
     private class StudentClick implements View.OnClickListener {
 
@@ -58,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(this.mAct, StudentActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent, INSERIR_ESTUDANTE);
         }
     }
 }
--- app/src/main/java/ibratec/recife/pe/br/helloworld/StudentActivity.java
+++ app/src/main/java/ibratec/recife/pe/br/helloworld/StudentActivity.java
@@ -1,13 +1,48 @@
 package ibratec.recife.pe.br.helloworld;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StudentActivity extends AppCompatActivity {
+
+    EditText editNomeStudent;
+    Button btnConfirma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+
+        editNomeStudent  = (EditText) findViewById(R.id.nomeStudent);
+
+        btnConfirma = (Button) findViewById(R.id.btnIncluirStudent);
+        btnConfirma.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+
+                   if (!editNomeStudent.getText().toString().equals("")) {
+                       Intent devolve = new Intent();
+                       devolve.putExtra(MainActivity.VAR_NOME_STUDENT, editNomeStudent.getText().toString());
+                       setResult(Activity.RESULT_OK, devolve);
+                       finish();
+                   } else {
+                       Toast toast = Toast.makeText(getApplicationContext(), "Digite um nome, boy!", Toast.LENGTH_SHORT);
+                       toast.show();
+                   }
+
+               }
+        });
+
     }
+
+
+
+
 }
